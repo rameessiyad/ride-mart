124f38cc54f942d243d51585794c7a90c12b6332
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-08-09 08:38:24 +0530

    header

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
+        <Header />
         <div className="min-h-screen">{children}</div>
         <footer className="bg-blue-50 py-12">
           <div className="container mx-auto text-center px-4 text-gray-600">
