a618178737d37c790590ccf8d309bde2c1cf7708
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-08-09 11:04:02 +0530

    clerk middleware setup

--- middleware.js
+++ middleware.js
@@ -1,6 +1,18 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+  "/admin(.*)",
+  "/saved-bikes(.*)",
+  "/reservation(.*)",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+  if (!userId && isProtectedRoute(req)) {
+    const { redirectToSignIn } = await auth();
+    return redirectToSignIn();
+  }
+});
 
 export const config = {
   matcher: [
