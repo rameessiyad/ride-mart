7ee67949a0f701933a8a347262365c5fcff2af23
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-08-09 10:48:35 +0530

    clerk setup

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -13,16 +14,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>
-        <Header />
-        <div className="min-h-screen">{children}</div>
-        <footer className="bg-blue-50 py-12">
-          <div className="container mx-auto text-center px-4 text-gray-600">
-            <p>Made with ❤️ by Ramees M Siyad</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} antialiased`}>
+          <Header />
+          <div className="min-h-screen">{children}</div>
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto text-center px-4 text-gray-600">
+              <p>Made with ❤️ by Ramees M Siyad</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
