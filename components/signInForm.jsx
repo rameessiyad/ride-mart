557902bea8e09032a987a184b97b881deac5de48
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-08-13 07:16:50 +0530

    handle submit func()

--- components/signInForm.jsx
+++ components/signInForm.jsx
@@ -1,18 +1,41 @@
-'use client';
+"use client";
 
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { useSignIn } from "@clerk/nextjs";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
 const SignInForm = () => {
-    const {signIn, isLoaded, error} = useSignIn();
+  const { signIn, isLoaded, error } = useSignIn();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!isLoaded) return;
+
+    setLoading(true);
+    try {
+      const result = await signIn.create({
+        identifier: email,
+        password,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+    setLoading(false);
+  };
   return (
     <div className="h-auto bg-white border-2 shadow-md rounded-md w-80 md:w-96">
       <div className="p-6 md:p-8">
-        <h1 className="text-2xl font-semibold text-center">Sign in to Ride Mart</h1>
-        <form action="">
+        <h1 className="text-2xl font-semibold text-center">
+          Sign in to Ride Mart
+        </h1>
+        <form onSubmit={handleSubmit}>
           <Input
             type="email"
             placeholder="Email Address"
@@ -25,10 +48,15 @@ const SignInForm = () => {
             className="w-full p-2 mt-4 border-2 rounded-md"
             required
           />
-          <Button variant="" className="w-full my-4">Sign In</Button>
+          <Button variant="" className="w-full my-4">
+            Sign In
+          </Button>
         </form>
         <p className="text-sm font-light text-center">
-          Don&apos;t have an account ? <Link href={"/sign-up"} className="text-blue-600 font-semibold">Sign Up</Link>
+          Don&apos;t have an account ?{" "}
+          <Link href={"/sign-up"} className="text-blue-600 font-semibold">
+            Sign Up
+          </Link>
         </p>
       </div>
     </div>
