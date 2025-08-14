70614a8932b219a3eac9d0c393bc80b63717ff3a
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-08-14 06:44:51 +0530

    admin button

--- components/header.jsx
+++ components/header.jsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
-import { Bike } from "lucide-react";
+import { ArrowLeft, Bike, Heart, Layout } from "lucide-react";
 
-const Header = async ({ isAdminPage = false }) => {
-  const isAdmin = false;
+const Header = async ({ isAdminPage = true }) => {
+  const isAdmin = true;
   return (
     <header className="fixed top-0 w-full bg-white/80 backdrop:blur-md z-50 border-b">
       <nav className="mx-auto p-4 flex items-center justify-between">
@@ -23,15 +23,44 @@ const Header = async ({ isAdminPage = false }) => {
           )}
         </Link>
 
-        <div>
-          <SignedIn>
-            <Link href={"/saved-bikes"}>
-              <Button>
-                <Bike size={18} />
-                <span className="hidden md:inline">Saved Bikes</span>
-              </Button>
-            </Link>
-          </SignedIn>
+        <div className="flex items-center space-x-2">
+          {isAdminPage ? (
+            <>
+              <Link href={"/"}>
+                <Button
+                  variant="outline"
+                  className="cursor-pointer flex items-center gap-2"
+                >
+                  <ArrowLeft size={18} />
+                  <span className="hidden md:inline">Back to App</span>
+                </Button>
+              </Link>
+            </>
+          ) : (
+            <SignedIn>
+              <Link href={"/saved-bikes"}>
+                <Button className="cursor-pointer">
+                  <Bike size={18} />
+                  <span className="hidden md:inline">Saved Bikes</span>
+                </Button>
+              </Link>
+              {!isAdmin ? (
+                <Link href={"/reservations"}>
+                  <Button variant="outline" className="cursor-pointer">
+                    <Heart size={18} />
+                    <span className="hidden md:inline">My Reservations</span>
+                  </Button>
+                </Link>
+              ) : (
+                <Link href="/admin">
+                  <Button variant="outline">
+                    <Layout size={18} />
+                    <span className="hidden md:inline">Admin Portal</span>
+                  </Button>
+                </Link>
+              )}
+            </SignedIn>
+          )}
         </div>
       </nav>
     </header>
