5317b1a2a5da57f5e8b30c1de4dd073a0d30d709
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-09-07 08:53:06 +0530

    css

--- app/page.js
+++ app/page.js
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="pt-20 flex flex-col">
+    <div className="pt-22 mt-4 flex flex-col">
       {/* Hero */}
-      <section>
-        <div>
-          <div>
+      <section className="relative py-16 md:py-28 dotted-background">
+        <div className="max-w-4xl mx-auto text-center">
+          <div className="mb-8">
             <h1>Find your dream bike with Ride Mart</h1>
             <p>
               Advanced AI Bike Search and test drive from thousands of bikes
