83021e3bd35b99b24005b467b4c84d5bd22db851
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-09-07 08:43:12 +0530

    landing page structure

--- app/page.js
+++ app/page.js
@@ -2,7 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="pt-20 flex flex-col">
+      {/* hero */}
+      <section>
+        <div>
+          <div>
+            <h1>Find your dream bike with Ride Mart</h1>
+            <p>
+              Advanced AI Bike Search and test drive from thousands of bikes
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
