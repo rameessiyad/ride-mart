6af57e2d9564f0f4b15cd4afc86fcac459530b93
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-09-13 16:19:55 +0530

    dummy images

--- components/bikeCard.jsx
+++ components/bikeCard.jsx
@@ -1,10 +1,25 @@
+
 import React from "react";
 import { Card, CardHeader } from "./ui/card";
+import Image from "next/image";
 
-const BikeCard = ({}) => {
+const BikeCard = ({ bike }) => {
   return (
     <Card>
-      <CardHeader></CardHeader>
+      <div className="relative h-48">
+        {bike.images && bike.images.length > 0 ? (
+          <div>
+            <Image
+              src={bike.images[0]}
+              alt={`${bike.make} ${bike.model}`}
+              fill
+              className="object-cover group-hover:scale-105 transition duration-300"
+            />
+          </div>
+        ) : (
+          <div>poda</div>
+        )}
+      </div>
     </Card>
   );
 };
