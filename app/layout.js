ae68aacaab86ffc78bbf014f2f1736fd33d546da
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-10-02 13:13:54 +0530

    feat: sonner toaster

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -18,7 +19,8 @@ export default function RootLayout({ children }) {
       <html lang="en">
         <body className={`${inter.className} antialiased`}>
           <Header />
-          <div className="min-h-screen">{children}</div>
+          <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto text-center px-4 text-gray-600">
               <p>Made with ❤️ by Ramees M Siyad</p>
