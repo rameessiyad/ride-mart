2cc5f5ecaa6dd1de3c90ee02704b0e353d752801
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-10-02 12:15:37 +0530

    searchbox component

--- components/homeSearch.jsx
+++ components/homeSearch.jsx
@@ -1,7 +1,22 @@
+'use client';
+
 import React from "react";
+import { Input } from "./ui/input";
 
 const HomeSearch = () => {
-  return <div>HomeSearch</div>;
+  const handleTextSubmit = (e) => {};
+  return (
+    <div>
+      <form onSubmit={handleTextSubmit}>
+        <div className="relative flex items-center">
+          <Input
+            type="text"
+            placeholder="Enter make, model, or use AI Image Search..."
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default HomeSearch;
