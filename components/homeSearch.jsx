4e71f3158312f6714e612553fdf1d6d8716e3dd8
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-10-02 12:27:01 +0530

    searchbox component

--- components/homeSearch.jsx
+++ components/homeSearch.jsx
@@ -1,9 +1,14 @@
-'use client';
+"use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "./ui/input";
+import { Camera } from "lucide-react";
+import { Button } from "./ui/button";
 
 const HomeSearch = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isImageSearchActive, setIsImageSearchActive] = useState(false);
+
   const handleTextSubmit = (e) => {};
   return (
     <div>
@@ -12,7 +17,25 @@ const HomeSearch = () => {
           <Input
             type="text"
             placeholder="Enter make, model, or use AI Image Search..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="pl-10 pr-32 md:pr-12 py-6 w-full rounded-full border-gray-300 bg-white/95 backdrop-blur-md"
           />
+
+          <div className="absolute right-2 flex items-center gap-1">
+            <Camera
+              size={35}
+              className="cursor-pointer rounded-xl p-1.5"
+              onClick={() => setIsImageSearchActive(!isImageSearchActive)}
+              style={{
+                background: isImageSearchActive ? "black" : "",
+                color: isImageSearchActive ? "white" : "",
+              }}
+            />
+            <Button type="submit" className="rounded-full">
+              Search
+            </Button>
+          </div>
         </div>
       </form>
     </div>
