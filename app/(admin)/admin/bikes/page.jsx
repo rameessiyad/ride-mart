64191f0f209a5d8b14e97f1717ce0941e2feb352
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-10-04 19:03:32 +0530

    bike management admin

--- app/(admin)/admin/bikes/page.jsx
+++ app/(admin)/admin/bikes/page.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import BikesList from "./_components/bike-list";
+
+export const metadata = {
+  title: "Bikes | Ride Mart Admin",
+  description: "Manage Bikes in your marketplace",
+};
 
 const BikesPage = () => {
   return (
-    <div>BikesPage</div>
-  )
-}
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-6">Bikes Management</h1>
+      <BikesList />
+    </div>
+  );
+};
 
-export default BikesPage
+export default BikesPage;
